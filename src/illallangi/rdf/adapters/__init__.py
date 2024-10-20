04aa9c76eb679333903846a8c926662a060d207d
Andrew Cole <andrew.cole@illallangi.com>
2024-10-21 10:09:52 +1100

    feat(EducationAdapter): added

--- src/illallangi/rdf/adapters/__init__.py
+++ src/illallangi/rdf/adapters/__init__.py
@@ -1,6 +1,9 @@
+from .aviation import AviationAdapter
+from .education import EducationAdapter
 from .residential import ResidentialAdapter
 
 __all__ = [
     "AviationAdapter",
+    "EducationAdapter",
     "ResidentialAdapter",
 ]
